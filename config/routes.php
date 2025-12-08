570dec0296a0306814a1ae0bac132438651770bb
Caique Martins <caiquemart16@gmail.com>
2025-12-07 22:58:10 -0300

    feat: add contacts routes

--- config/routes.php
+++ config/routes.php
@@ -5,4 +5,8 @@ use FastRoute\RouteCollector;
 return function (RouteCollector $r) {
     $r->post('/users', [\App\Infrastructure\Http\Controller\CreateUserController::class, 'handle']);
     
+    $r->post('/contacts', [\App\Infrastructure\Http\Controller\CreateContactController::class, 'handle']);
+    $r->get('/contacts', [\App\Infrastructure\Http\Controller\ListContactsController::class, 'handle']);
+    $r->put('/contacts/{id:\d+}', [\App\Infrastructure\Http\Controller\UpdateContactController::class, 'handle']);
+    $r->delete('/contacts/{id:\d+}', [\App\Infrastructure\Http\Controller\DeleteContactController::class, 'handle']);
 };
