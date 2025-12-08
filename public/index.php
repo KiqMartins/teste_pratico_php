80dfbff1e79366b84f0f235d440f864768f30dae
Caique Martins <caiquemart16@gmail.com>
2025-12-07 21:25:10 -0300

    feat/refactor: modify index file to use routes methods

--- public/index.php
+++ public/index.php
@@ -1,8 +1,43 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+use FastRoute\Dispatcher;
+use Laminas\Diactoros\ServerRequestFactory;
 
-echo json_encode([
-    'status' => 'success', 
-    'message' => 'Initial test'
-]);
+$container = require __DIR__ . '/../config/bootstrap.php';
+
+$dispatcher = FastRoute\simpleDispatcher(require __DIR__ . '/../config/routes.php');
+
+$request = ServerRequestFactory::fromGlobals();
+
+$routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getUri()->getPath());
+
+switch ($routeInfo[0]) {
+    case Dispatcher::NOT_FOUND:
+        http_response_code(404);
+        echo json_encode(['error' => 'Not Found']);
+        break;
+        
+    case Dispatcher::METHOD_NOT_ALLOWED:
+        http_response_code(405);
+        echo json_encode(['error' => 'Method Not Allowed']);
+        break;
+        
+    case Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        
+        [$class, $method] = $handler;
+        
+        $controller = $container->get($class);
+        
+        $response = $controller->$method($request, $vars);
+        
+        http_response_code($response->getStatusCode());
+        foreach ($response->getHeaders() as $name => $values) {
+            foreach ($values as $value) {
+                header(sprintf('%s: %s', $name, $value), false);
+            }
+        }
+        echo $response->getBody();
+        break;
+}
