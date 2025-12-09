251195de8d95b84d29b5c00c16ae3c412cf435f8
Caique Martins <caiquemart16@gmail.com>
2025-12-09 06:10:04 -0300

    feat: set custom base url through env

--- frontend/src/http/api.ts
+++ frontend/src/http/api.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosInstance, type InternalAxiosRequestConfig } from 'axios';
 
 const api: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
